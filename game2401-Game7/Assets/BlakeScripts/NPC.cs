e3b82975c585dbbfa3e6d2b4de7a05b7ba2cf1cd
Blake Laprade <lapradeblake@gmail.com>
2023-12-06 13:09:36 -0700

    continued work on state machine

--- game2401-Game7/Assets/BlakeScripts/NPC.cs
+++ game2401-Game7/Assets/BlakeScripts/NPC.cs
@@ -7,16 +7,20 @@ public class NPC : MonoBehaviour
     private AIPath _path;
     private AIDestinationSetter _destinationSetter;
     [SerializeField] private Transform[] _patrolPath;
-    private int _routeIndex = 0;
+    private int _routeIndex = 0; //the index of the transform the NPC should path to while on patrol
 
     private Coroutine _currentState;
 
     [SerializeField] private float _speed = 1f;
+
+    [SerializeField] private GameObject _visionCone;
+    private Rigidbody2D _rb;
     private void Awake()
     {
         _path = GetComponent<AIPath>();
         _destinationSetter = GetComponent<AIDestinationSetter>();
         _path.maxSpeed = _speed;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void SetState(IEnumerator newState) //when we change states, we stop our previous coroutine and then initialize a new one. Technically this can be done with "StopAllCoroutines()" because we only have one coroutine running, but in the case that we had more, we will only a stop a specific coroutine.
@@ -31,17 +35,33 @@ public class NPC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetState(OnPatrol());
+       // SetState(OnPatrol());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //_visionCone.transform.LookAt(_destinationSetter.target.position);
     }
 
     IEnumerator OnPatrol()
     {
         yield return new WaitForFixedUpdate();
+        _destinationSetter.target = _patrolPath[_routeIndex];
+        if(_routeIndex == _patrolPath.Length)
+        {
+            _routeIndex = 0;
+        }
+        else
+        {
+            _routeIndex++;
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hi");
+    }
+
+
 }
