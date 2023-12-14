46dde4bf513666cbefcf4ae9daa43c4d41d4a6f8
ShaylynKM <shaylynmelin@gmail.com>
2023-12-14 13:41:43 -0700

    WIP pause menu

--- game2401-Game7/Assets/ShayScripts/PauseMenu.cs
+++ game2401-Game7/Assets/ShayScripts/PauseMenu.cs
@@ -1,18 +1,100 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _pauseMenuScreen;
+
+    private bool _gamePaused;
+
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private GameObject _resumeButton;
+
+    [SerializeField]
+    private GameObject _mainMenuButton;
+
+    [SerializeField]
+    private GameObject _exitButton;
+
+
+    private void Awake()
+    {
+        _player = GameObject.Find("Player"); // Reference to the player
+        _resumeButton = GameObject.Find("Resume_Button"); // Reference to the resume button
+        _mainMenuButton = GameObject.Find("MainMenu_Button"); // Reference to the main menu button
+        _exitButton = GameObject.Find("Quit_Button"); // Reference to the exit button
+    }
+
     void Start()
     {
+        _pauseMenuScreen.SetActive(false); // Hides the pause menu
+    }
+
+    void Update()
+    {
+        if (_gamePaused)
+        {
+          _player?.SetActive(false); // Deactivate the Player object if it exists in the scene. Keeps the player character from recieving any inputs while the game is paused.
+        }
+        else if (!_gamePaused)
+        {
+           _player?.SetActive(true); // Activate the Player object if it exists in the scene
+        }
+    }
+
+    #region Button Actions
+
+    public void OnResumePress()
+    {
+        GameResume(); // Resumes the game when the Resume button is pressed.
+    }
+
+    public void OnMainMenuPress()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu"); // Loads the Main Menu scene
+    }
+
+    public void OnExitPress()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit(); // Quits the game
+    }
+
+    #endregion
+
+    public void GamePause()
+    {
+        _gamePaused = true;
+
+        OpenMenu();
+
+        Time.timeScale = 0f; // Freezes time
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameResume()
     {
-        
+        _gamePaused = false;
+
+        CloseMenu();
+
+        Time.timeScale = 1f; // Resumes time
+    }
+
+    private void OpenMenu()
+    {
+        _pauseMenuScreen.SetActive(true); // Shows the pause menu
+    }
+
+    private void CloseMenu()
+    {
+        _pauseMenuScreen.SetActive(false); // Hide the pause menu
     }
 }
